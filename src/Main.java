b001dafbef23c5bbebccdccd4b6f6cf0d92ff02f
Jean Semolini <jean.maiko@hotmail.com>
2018-03-14 00:38:32 -0300

    Questao 08 v1

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,38 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int numeroA, numeroB, numeroC;	
+		int numeroA, numeroB, numeroC;
 		String tamanhoPrimeiro, tamanhoSegundo, numeroCriado, letra1, letra2;
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Digite o número A: ");
 		numeroA = sc.nextInt();
-		
+
 		System.out.print("Digite o número B: ");
 		numeroB = sc.nextInt();
-				
+
 		tamanhoPrimeiro = Integer.toString(numeroA);
-		tamanhoSegundo = Integer.toString(numeroB);		
-		
+		tamanhoSegundo = Integer.toString(numeroB);
+
 		numeroCriado = "";
 		letra1 = "";
 		letra2= "";
-			
-		for (int i = 1; i <= (tamanhoPrimeiro.length() > tamanhoSegundo.length() ? 
+
+		for (int i = 1; i <= (tamanhoPrimeiro.length() > tamanhoSegundo.length() ?
 								tamanhoPrimeiro.length() : tamanhoSegundo.length()); i++) {
-			
+
 			if (tamanhoPrimeiro.length() >= i) {
 				letra1 = Integer.toString(numeroA).substring(i-1, i);
 				numeroCriado += letra1;
 			}
 			if (tamanhoSegundo.length() >= i) {
 				letra2 = Integer.toString(numeroB).substring(i-1, i);
-				numeroCriado += letra2;	
+				numeroCriado += letra2;
 			}
-		}	
-		
+		}
 		numeroC = Integer.parseInt(numeroCriado);
-		
-		System.out.println((numeroC > 1000000 ? -1 : "Número C: " + numeroC));		
-		
+
+		System.out.println((numeroC > 1000000 ? -1 : "Número C: " + numeroC));
 	}
 
 }
